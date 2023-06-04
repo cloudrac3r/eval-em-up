6b5402cd12b30e2b615e16b3520278529648cec0
Cadence Ember <cadence@disroot.org>
2023-06-04 23:09:32 +1200

    add enemy shots. game is fun now.

--- raylib.rkt
+++ raylib.rkt
@@ -10,6 +10,14 @@
   (set! var (add1 var)))
 
 
+(define (rectangles-touching? x1 y1 w1 h1 x2 y2 w2 h2)
+  (and
+   (or (<= x1 x2 (+ x1 w1))
+       (<= x2 x1 (+ x2 w2)))
+   (or (<= y1 y2 (+ y1 h1))
+       (<= y2 y1 (+ y2 h2)))))
+
+
 ;;; Important constants
 (define screen-width 1280)
 (define screen-height 720)
@@ -46,6 +54,7 @@
 (define health-background-tex (delay (LoadTexture "tex/health-background.png")))
 (define health-frame-tex (delay (LoadTexture "tex/health-frame.png")))
 (define health-bars-tex (delay (LoadTexture "tex/health-bars.png")))
+(define enemy-shot-tex (delay (LoadTexture "tex/enemy-shot-sheet.png")))
 
 
 (define (eval-mixin %)
@@ -98,6 +107,7 @@
     (init-field height)
     (init-field scale)
     (init-field sprite-tex)
+    (init-field hitboxes)
     (init-field [sprites-across 1])
     (init-field [sprites-in-sheet 1])
     (init-field [sprite-index 0])
@@ -114,16 +124,35 @@
                       (make-Rectangle x y (* width scale) (* height scale)) ;; dest
                       (make-Vector2 0.0 0.0) ;; origin
                       0.0 ;; rotation
-                      WHITE))
+                      WHITE)
+      (when debug-mode
+        (for ([hitbox hitboxes])
+          (DrawRectangleRec
+           (make-Rectangle
+            (+ x (* scale (vector-ref hitbox 0)))
+            (+ y (* scale (vector-ref hitbox 1)))
+            (* scale (vector-ref hitbox 2))
+            (* scale (vector-ref hitbox 3)))
+           (make-Color 255 0 0 150)))))
 
     (define/public (touching? that)
-      (and
-       (or (<= (get-field x this) (get-field x that) (+ (get-field x this) (get-field width this)))
-           (<= (get-field x that) (get-field x this) (+ (get-field x that) (get-field width that))))
-       (or (<= (get-field y this) (get-field y that) (+ (get-field y this) (get-field height this)))
-           (<= (get-field y that) (get-field y this) (+ (get-field y that) (get-field height that)))))
-      #;(and ((abs (- (get-field x this) (get-field x that))) . < . 10)
-           ((abs (- (get-field y this) (get-field y that))) . < . 10)))))
+      (define sprites-touching?
+        (rectangles-touching?
+         (get-field x this) (get-field y this) (* scale (get-field width this)) (* scale (get-field height this))
+         (get-field x that) (get-field y that) (* scale (get-field width that)) (* scale (get-field height that))))
+      (if (not sprites-touching?)
+          #f
+          (for*/or ([hitbox1 (get-field hitboxes this)]
+                    [hitbox2 (get-field hitboxes that)])
+            (rectangles-touching?
+             (+ (get-field x this) (* scale (vector-ref hitbox1 0)))
+             (+ (get-field y this) (* scale (vector-ref hitbox1 1)))
+             (* scale (vector-ref hitbox1 2))
+             (* scale (vector-ref hitbox1 3))
+             (+ (get-field x that) (* scale (vector-ref hitbox2 0)))
+             (+ (get-field y that) (* scale (vector-ref hitbox2 1)))
+             (* scale (vector-ref hitbox2 2))
+             (* scale (vector-ref hitbox2 3))))))))
 
 (define (flipper-mixin %)
   ;; (printf "building flipper-mixin off ~v~n" %)
@@ -152,15 +181,24 @@
                [sprites-across 2]
                [sprites-in-sheet 2]
                [sprite-tex tex]
-               [flipper-ticks 20])
+               [flipper-ticks 20]
+               [hitboxes '(#(152 125 54 155)
+                           #(206 60 39 275)
+                           #(245 100 20 205)
+                           #(265 155 15 125)
+                           #(280 170 75 90)
+                           #(355 196 37 36))])
     (inherit-field x y width height scale)
-    (field [speed 4]
+    (field [speed 5]
            [last-shot 0]
            [shot-spacing 180]
-           [hp 2]
+           [hp 5]
            [health-gauge (new health-gauge%
                               [offset-x 60]
                               [offset-y 190])])
+
+    (define (damage)
+      (set! hp (sub1 hp)))
 
     (define/override (tick)
       (super tick)
@@ -177,7 +215,12 @@
         (when ((current-milliseconds) . > . (+ last-shot shot-spacing))
           (for ([ys (list 178.0 230.0)])
             (new shot% [x (+ x (* (- width 70) scale))] [y (+ y (* ys scale))]))
-          (set! last-shot (current-milliseconds)))))
+          (set! last-shot (current-milliseconds))))
+      (for ([entity (in-vector (es))])
+        (when (and (or (is-a? entity enemy%) (is-a? entity enemy-shot%))
+                   (send this touching? entity))
+          (damage)
+          (send entity die))))
 
     (define/override (draw)
       (super draw)
@@ -191,7 +234,8 @@
                [width 74.0]
                [height 20.0]
                [scale 0.5]
-               [sprite-tex shot-tex])
+               [sprite-tex shot-tex]
+               [hitboxes '(#(4 1 67 18))])
     (inherit-field x y width height)
 
     (define speed 8)
@@ -205,6 +249,69 @@
           (send this die)))
       (when (x . > . screen-width)
         (send this die)))))
+
+
+(define (die-offscreen-mixin %)
+  (class %
+    (super-new)
+    (inherit-field x y width height scale)
+
+    (define/override (tick)
+      (super tick)
+      (when (or ((+ x (* scale width)) . < . 0)
+                ((+ y (* scale height)) . < . 0)
+                (x . > . screen-width)
+                (y . > . screen-height))
+        (send this die)))))
+
+
+;;; Shots fired by the enemy ships.
+(define enemy-shot%
+  (class (die-offscreen-mixin (flipper-mixin (sprite-mixin entity%)))
+    (super-new [order 90]
+               [width 49.0]
+               [height 44.0]
+               [scale 0.5]
+               [sprites-across 2]
+               [sprites-in-sheet 2]
+               [flipper-ticks 15]
+               [sprite-tex enemy-shot-tex]
+               [hitboxes '(#(7 11 8 25)
+                           #(15 5 23 36)
+                           #(38 10 5 21))])
+    (inherit-field x y)
+    (define ship
+      (for/first ([e (es)]
+                  #:when (is-a? e ship%))
+        e))
+    (define speed 4)
+    ;; I forgot the trig calculations to do this, so time for a funny hack with complex numbers
+    (define velocity-complex (make-polar speed (angle (make-rectangular (- (get-field x ship) x) (- (get-field y ship) y)))))
+    (define velocity-x (real-part velocity-complex))
+    (define velocity-y (imag-part velocity-complex))
+
+    (define/override (tick)
+      (super tick)
+      (set! x (+ x velocity-x))
+      (set! y (+ y velocity-y)))))
+
+
+
+;;; Mixin for an enemy that regularly fires directly at the player
+(define (shot-regular-mixin %)
+  (class %
+    (super-new)
+    (inherit-field x y)
+    (init-field shot-time-ticks)
+    (init-field shot-class%)
+    (define shot-time-count 0)
+
+    (define/override (tick)
+      (super tick)
+      (set! shot-time-count (add1 shot-time-count))
+      (when (shot-time-count . >= . shot-time-ticks)
+        (new shot-class% [x x] [y y])
+        (set! shot-time-count 0)))))
 
 
 (define health-gauge%
@@ -243,15 +350,15 @@
                       WHITE))))
 
 
+
 ;;; An enemy that can be shot and attacked to evaluate its code. Most enemies will simply have their code as (die).
 (define enemy%
-  (class (flipper-mixin (sprite-mixin entity%))
+  (class (die-offscreen-mixin (flipper-mixin (sprite-mixin entity%)))
     (super-new [order 50])
     (inherit-field x y)
 
     (field [shots-taken 0]
            [can-be-hit-this-tick? #t])
-    (define speed -2)
     (field [base-command "die"])
 
     (define command (string-append "(" base-command ")"))
@@ -267,10 +374,7 @@
 
     (define/override (tick)
       (super tick)
-      (set! can-be-hit-this-tick? #t)
-      (set! x (+ x speed))
-      (when (x . < . 0)
-        (send this die)))
+      (set! can-be-hit-this-tick? #t))
 
     (define/public (damage)
       (when can-be-hit-this-tick?
@@ -282,15 +386,29 @@
 
 
 (define enemy-basic%
-  (class enemy%
+  (class (shot-regular-mixin enemy%)
     (super-new [width 225.0]
                [height 121.0]
                [scale 0.5]
                [sprite-tex basic-tex]
                [sprites-across 3]
                [sprites-in-sheet 3]
-               [flipper-ticks 6])
+               [flipper-ticks 6]
+               [shot-time-ticks 110]
+               [shot-class% enemy-shot%]
+               [hitboxes '(#(20 56 52 13)
+                           #(72 42 23 37)
+                           #(95 35 85 50)
+                           #(114 17 39 18)
+                           #(115 85 37 16)
+                           #(188 41 20 30))])
     (inherit-field x y shots-taken display-text)
+
+    (define speed -2)
+
+    (define/override (tick)
+      (super tick)
+      (set! x (+ x speed)))
 
     (define/override (draw)
       (super draw)
@@ -308,8 +426,19 @@
                [sprite-tex clunker-tex]
                [sprites-across 2]
                [sprites-in-sheet 2]
-               [flipper-ticks 30])
+               [flipper-ticks 30]
+               [hitboxes '(#(54 75 21 71)
+                           #(75 66 98 89)
+                           #(95 38 66 28)
+                           #(98 155 62 25)
+                           #(173 68 30 82))])
     (inherit-field x y shots-taken display-text)
+
+    (define speed -1.5)
+
+    (define/override (tick)
+      (super tick)
+      (set! x (+ x speed)))
 
     (define/override (draw)
       (super draw)
@@ -349,7 +478,8 @@
                [scale 0.5]
                [x 0.0]
                [y 0.0]
-               [sprite-tex background-tex])
+               [sprite-tex background-tex]
+               [hitboxes null])
     (inherit-field x y width height scale)
     (init-field i)
 
@@ -376,7 +506,7 @@
   (SetTargetFPS 60)
 
   ;; must initialise window (and opengl context) before any textures can be loaded
-  (InitWindow screen-width screen-height "Basic Window")
+  (InitWindow screen-width screen-height "Eval-Em-Up!")
 
   (collect-garbage)
 
